19c3c2514efcc0f4e0fce87f7852b48c98bee51a
Banana <git@imabanana80.com>
2026-08-28 17:10:59 +0800

    chore: have nixos wsl share nixpkgs

--- flake.nix
+++ flake.nix
@@ -6,7 +6,10 @@
             url = "github:nix-community/home-manager";
             inputs.nixpkgs.follows = "nixpkgs";
         };
-        nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
+        nixos-wsl = {
+            url = "github:nix-community/NixOS-WSL/main";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
         zen-browser = {
             url = "github:0xc000022070/zen-browser-flake";
             inputs = {
